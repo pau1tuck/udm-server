17f28419f6d793dbda98b1a87c304173d200326d
Paul Tuck <paul.tuck@oxon.org>
2023-05-06 22:17:41 +0700

    "2023-05-06_22:17_UTC+7"

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -1,3 +1,21 @@
+use juniper::RootNode;
+
+struct Todo {
+    id: i32,
+    track_id: String,
+    artist: String,
+    title: String,
+    version: String,
+    label: String,
+    month: u8,
+    year: u8,
+    duration: String,
+    buy_url: String,
+    votes: u32,
+    created_at: String,
+    updated_at: String,
+}
+
 fn main() {
     println!("Hello, World!");
 }
