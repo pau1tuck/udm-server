2c6392b9c00e249100c7c6ce9f876f887243f127
Paul Tuck <paul.tuck@oxon.org>
2023-05-12 02:41:36 +0700

    "2023-05-12_02:41_UTC+7"

---
+++ server-apollo/src/types/track.interface.ts
@@ -0,0 +1,14 @@
+export interface ITrack {
+    id: number;
+    trackId: string;
+    artist: string;
+    title: string;
+    version: string;
+    label: string;
+    month: number;
+    year: number;
+    buyUrl: string;
+    votes: number;
+    createdAt: string;
+    updatedAt: string;
+}
