529ff86125202670a1243c6774cb0e8d39f79982
Paul Tuck <paul.tuck@oxon.org>
2023-05-13 09:58:47 +0700

    "2023-05-13_09:58+07"

--- server-apollo/scripts/startDocker.sh
+++ server-apollo/scripts/startDocker.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Load environment variables from .env file
+set -a
+source .env
+set +a
+
+# Build the Docker image using the Dockerfile
+docker build -t udm-server-apollo .
+
 # Start the Docker container with PostgreSQL
-docker run --rm --name udm-server-apollo -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=badpassword -p 5432:5432 -d postgres:latest
+docker run --rm --name udm-server-apollo -e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PASSWORD -p $DB_PORT -d postgres:latest
 
 # Wait for the PostgreSQL container to start
 sleep 5
 
 # Perform a database backup
-pg_dump -U admin -h localhost -p 5432 udm-server-apollo > backup.sql
+pg_dump -U $DB_USER -h localhost -p $DB_PORT $DB_NAME > backup.sql
---
+++ server-apollo/src/types/playlist.interface.ts
@@ -0,0 +1,8 @@
+export interface IPlaylist {
+    id: string;
+    userId: string;
+    name: string;
+    tracks: string[];
+    createdAt: string;
+    updatedAt: string;
+}
