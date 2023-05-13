f0a104e1de220cd3758d75800162940d7a4fad90
Paul Tuck <paul.tuck@oxon.org>
2023-05-13 10:35:30 +0700

    "2023-05-13_10:35+07"

---
+++ server-apollo/backup.sql
@@ -0,0 +1,1 @@
+pg_dump: error: connection to server on socket "/var/run/postgresql/.s.PGSQL.5432" failed: FATAL:  role "-d" does not exist
--- server-apollo/scripts/startDocker.sh
+++ server-apollo/scripts/startDocker.sh
@@ -5,14 +5,28 @@ set -a
 source .env
 set +a
 
-# Build the Docker image using the Dockerfile
-docker build -t udm-server-apollo .
+# Remove existing udm-server-apollo container
+echo "Removing existing udm-server-apollo container..."
+docker stop udm-server-apollo
+docker rm udm-server-apollo
+
+# Build the Docker image using the Dockerfile if it doesn't exist
+if [[ "$(docker images -q udm-server-apollo 2> /dev/null)" == "" ]]; then
+    docker build -t udm-server-apollo .
+fi
 
 # Start the Docker container with PostgreSQL
-docker run --rm --name udm-server-apollo -e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PASSWORD -p $DB_PORT -d postgres:latest
+echo "Starting udm-server-apollo container..."
+docker run --name udm-server-apollo -e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PASS -e POSTGRES_DB=$DB_NAME -p $DB_PORT:5432 -d postgres:latest
 
 # Wait for the PostgreSQL container to start
+echo "Waiting for PostgreSQL container to start..."
 sleep 5
 
+# Create the database if it doesn't exist
+echo "Creating database $DB_NAME..."
+docker exec -it udm-server-apollo psql -U $DB_USER -c "DO \$\$ BEGIN CREATE DATABASE $DB_NAME; EXCEPTION WHEN duplicate_database THEN END; \$\$"
+
 # Perform a database backup
-pg_dump -U $DB_USER -h localhost -p $DB_PORT $DB_NAME > backup.sql
+echo "Performing database backup..."
+docker exec -it udm-server-apollo pg_dump -U $DB_USER -d $DB_NAME > backup.sql
