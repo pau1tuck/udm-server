076a6ea489637cb2f1a7146f087e319b592a109f
Paul Tuck <p6ul.tuck@gmail.com>
2023-05-18 12:39:46 +0700

    "2023-05-18_12:39SEAST"

--- server-apollo/scripts/mergeSchemas.cjs
+++ server-apollo/scripts/mergeSchemas.cjs
@@ -4,11 +4,21 @@ const { mergeTypeDefs } = require("@graphql-tools/merge");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { print } = require("graphql");
 
-const schemaFiles = join(__dirname, "../src/graphql/*.graphql");
-const loadedFiles = loadFilesSync(schemaFiles);
+// Define the schema files directory based on the operating system
+const schemaFilesDir =
+    process.platform === "win32"
+        ? join(__dirname, "..\\src\\graphql\\*.graphql") // Windows
+        : join(__dirname, "../src/graphql/*.graphql"); // Mac/Linux
+
+const loadedFiles = loadFilesSync(schemaFilesDir);
 const mergedSchema = mergeTypeDefs(loadedFiles);
 
-const outputPath = join(__dirname, "../src/graphql/schema.graphql");
+// Define the output path based on the operating system
+const outputPath =
+    process.platform === "win32"
+        ? join(__dirname, "..\\src\\graphql\\schema.graphql") // Windows
+        : join(__dirname, "../src/graphql/schema.graphql"); // Mac/Linux
+
 writeFileSync(outputPath, print(mergedSchema));
 
 console.log(`Merged GraphQL schema saved to ${outputPath}`);
