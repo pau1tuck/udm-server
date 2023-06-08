c5dd4e91cc8088a64184655bd8a06ba0a15e08a8
Paul Tuck <paul.tuck@oxon.org>
2023-06-09 00:20:06 +0700

    "2023-06-09_00:20_UTC+7"

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+// ConfigModule exposes a ConfigService to load the dotenv file:
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot()], // Environment variable key-value pairs are parsed and resolved
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ server-nest/src/config/environment.config.ts
@@ -0,0 +1,10 @@
+export default () => ({
+    port: Number(process.env.PORT) || 5000,
+    database: {
+        host: process.env.DB_HOST || "127.0.0.1",
+        port: Number(process.env.DB_PORT) || 5432,
+        name: process.env.DB_NAME || "test",
+        user: process.env.DB_USER || "postgres",
+        password: process.env.DB_PASS || "postgres",
+    },
+});
