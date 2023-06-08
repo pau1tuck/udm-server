650898509c57fab4bbcf4e13dab133745aae303a
Paul Tuck <paul.tuck@oxon.org>
2023-06-08 22:17:38 +0700

    "2023-06-08_22:17_UTC+7"

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+const HOST = process.env.NODE_ENV === "development" ? "localhost" : "0.0.0.0";
+const PORT = process.env.PORT || 5000;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(5000);
+    console.log(`\nServer running on ${HOST}:${PORT}`);
 }
 bootstrap();
