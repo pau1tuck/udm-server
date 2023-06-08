d189b23ce18e100c1b7f543c10b08a7f12727276
Paul Tuck <paul.tuck@oxon.org>
2023-06-09 00:21:45 +0700

    "2023-06-09_00:21_UTC+7"

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common";
 // ConfigModule exposes a ConfigService to load the dotenv file:
+import environment from "@/config/environment.config";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-    imports: [ConfigModule.forRoot()], // Environment variable key-value pairs are parsed and resolved
+    imports: [
+        ConfigModule.forRoot({
+            load: [environment],
+        }),
+    ], // Environment variable key-value pairs are parsed and resolved
     controllers: [AppController],
     providers: [AppService],
 })
